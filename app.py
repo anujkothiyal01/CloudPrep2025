8083304064dfafe01285d6f1e1e0348dba619779
Anuj Kothiyal <92199099+anujkothiyal01@users.noreply.github.com>
2025-03-20 17:55:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-GMAIL_USER = 'kothiyal.official@gmail.com'
-GMAIL_PASSWORD = 'xetv zaze ckcp sfjk'
+GMAIL_USER = "kothiyal.official@gmail.com"
+GMAIL_PASSWORD = "xetv zaze ckcp sfjk"
 
 
 # User Model (Added is_admin)
